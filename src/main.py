53fb8721dc0e7883bfa5175bfd1d48970ae64dd6
Nicola Santi <nicola@nicolasanti.it>
2025-07-20 15:14:41 +0200

    Fix script output

--- src/main.py
+++ src/main.py
@@ -12,39 +12,43 @@ model = keras.Sequential([
 
 #model.summary()
 
-# Imposta pesi fissi per esempio (opzionale)
-w1 = np.array([[0.1, 0.3, 0.5],
-               [0.2, 0.4, 0.6]])  # shape (2,3)
+# set weights as in the article
+w1 = np.array([[0.1, 0.2],
+               [0.3, 0.4],
+               [0.5, 0.6]])
 
-w2 = np.array([[0.1, 0.4],
-               [0.2, 0.5],
-               [0.3, 0.6]])       # shape (3,2)
+w2 = np.array([[0.1, 0.2, 0.3],
+               [0.4, 0.5, 0.6]])
 
-model.layers[0].set_weights([w1])
-model.layers[1].set_weights([w2])
+model.layers[0].set_weights([w1.T])
+model.layers[1].set_weights([w2.T])
 
-# Controllo che siano impostati correttamente
-print("Layer 1 weights:\n", model.layers[0].get_weights()[0].T)
-print("Layer 2 weights:\n", model.layers[1].get_weights()[0].T)
+# show weights just set
+print("--- Weights")
+print("Layer 1:\n", model.layers[0].get_weights()[0].T, "\n")
+print("Layer 2:\n", model.layers[1].get_weights()[0].T, "\n")
 
+# set also a fake data point and label
+x = tf.constant([[1.0, 2.0]])
+y_true = tf.constant([[1.0, 1.15]])
 
-# Singolo input e output atteso
-x = tf.constant([[1.0, 2.0]])     # Input: (1, 2)
-y_true = tf.constant([[1.0, 1.15]])     # Output atteso: (1, 1.15)
-
-# Calcolo del gradiente
+# setup auto-differentiation
 with tf.GradientTape() as tape:
-    # Assicura che il tape tenga traccia dei pesi
-    #tape.watch(model.trainable_variables)
-
     y_pred = model(x)                                     # Forward pass
-    print("Output del primo livello:", model.layers[0](x).numpy())
-    print("Output del secondo livello:", y_pred)                                         # [0.48693424 1.1463418 ]
     loss = tf.reduce_mean(tf.square(y_true - y_pred))     # MSE (L2)
 
-# Calcolo del gradiente della loss rispetto ai pesi
+# Compute gradient
 grads = tape.gradient(loss, model.trainable_variables)
 
-# Stampa i gradienti per ciascun layer
-for i, grad in enumerate(grads):
-    print(f"\nGradiente per i pesi del layer {i+1}:\n{grad.numpy().T}")
+# show gradients for our datapoint (iteration)
+#for i, grad in enumerate(grads):
+#    print(f"\nGradient layer {i+1}:\n{grad.numpy().T}")
+
+# # show gradients for our datapoint (direct)
+print("--- Input:", x.numpy(), "\n")
+print("--- Gradients")
+print(f"Layer 1:\n{grads[0].numpy().T}")
+print("Output:", model.layers[0](x).numpy(), "\n")
+print(f"Layer 2:\n{grads[1].numpy().T}")
+print("Output:", y_pred.numpy(), "\n")
+print("--- Label:", y_true.numpy(), "\n")
